93983e6da9c5a605f7f0e85da1e2285adf3c548c
dejoyjnr <dejoy@mail.de>
2018-04-04 17:34:54 +0200

    first ensemble implemented

--- housing.py
+++ housing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, KFold
 from sklearn.metrics import mean_absolute_error, make_scorer, mean_squared_error
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
 from sklearn.preprocessing import LabelEncoder, Imputer
@@ -12,14 +12,36 @@ from sklearn.ensemble.partial_dependence import partial_dependence, plot_partial
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.linear_model import BayesianRidge, LinearRegression, Ridge, Lasso, ElasticNet, Lars, LassoLars
+from sklearn.linear_model import BayesianRidge, LinearRegression, Ridge, Lasso, ElasticNet, Lars, LassoLars, LassoCV, LassoLarsCV
 from sklearn.linear_model import OrthogonalMatchingPursuit, HuberRegressor, RANSACRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from scipy.stats import skew
-import datetime
-from sklearn.model_selection import GridSearchCV
+import operator
+from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
+
+
+models = [LinearRegression(),
+          Ridge(),
+          ElasticNet(),
+          Lasso(),
+          # Lars(),
+          LassoLars(),
+          BayesianRidge(),
+          HuberRegressor(),
+          RANSACRegressor(),
+          OrthogonalMatchingPursuit(),
+          DecisionTreeRegressor(),
+          RandomForestRegressor(),
+          ExtraTreesRegressor(),
+          KNeighborsRegressor(),
+          GradientBoostingRegressor(),
+          XGBRegressor(),
+          SVR(),
+          MLPRegressor()]
 
 def housing():
+
     class DataFrameImputer(TransformerMixin):
 
         def __init__(self):
@@ -78,21 +100,9 @@ def housing():
     # for c in cols_object:
     #     preds.append(c)
     #
-    # models = [KNeighborsRegressor(), LinearRegression(), GradientBoostingRegressor(), XGBRegressor(), Ridge(), Lasso(),
-    #          ElasticNet(), Lars(), LassoLars(), BayesianRidge(), HuberRegressor(),
-    #           RANSACRegressor()]
     # models_names = ['KNeighborsRegressor', 'LinearRegression', 'GradientBoostingRegressor', 'XGBRegressor', 'Ridge', 'Lasso',
     #          'ElasticNet', 'Lars', 'LassoLars', 'BayesianRidge', 'HuberRegressor',
     #           'RANSACRegressor']
-    #
-    # def model_finder(X, y):
-    #     result = {}
-    #     for col in X.columns.tolist():
-    #         score = []
-    #         for model in models:
-    #             score.append((-1*cross_val_score(model, encoded_X[[col]], y, scoring = RMSE, cv=5).mean()))
-    #         result[col] = (models_names[score.index(max(score))], max(score))
-    #     return result
     #
     # # print(model_finder(encoded_X[preds], y))
     #
@@ -131,11 +141,8 @@ def housing():
     RMSE = make_scorer(mean_squared_error_, greater_is_better=False)
 
 
-    def create_submission(prediction, score):
-        now = datetime.datetime.now()
-        sub_file = 'submission_' + str(score) + '_' + str(now.strftime("%Y-%m-%d-%H-%M")) + '.csv'
-        print('Creating submission: ', sub_file)
-        pd.DataFrame({'Id': test['Id'].values, 'SalePrice': prediction}).to_csv(sub_file, index=False)
+    def create_submission(prediction):
+        pd.DataFrame({'Id': test['Id'].values, 'SalePrice': prediction}).to_csv(file_path+'sub.csv', index=False)
 
 
     def data_preprocess(train, test):
@@ -245,6 +252,7 @@ def housing():
         y_pred = model.predict(Xtest)
         return y_pred, -model.best_score_
 
+
     def grid_searcher(Xtrain, Xtest, ytrain, estimator, param_grid=None):
         if param_grid is None: param_grid = {}
         model = GridSearchCV(estimator=estimator, param_grid=param_grid, n_jobs=1, cv=5, scoring=RMSE, verbose=5)
@@ -258,30 +266,201 @@ def housing():
         return y_pred, -model.best_score_
 
 
+    def untuned_model_finder(X_train, y_train, models, scoring, cv, threshold, small_is_good=True):
+        all_models = {}
+        good_models = {}
+        for model in models:
+            name = str(model.__class__)
+            score = -cross_val_score(model, X_train, y_train, scoring=scoring, cv=cv).mean()
+            all_models[name[8:len(name)-2].split(".")[-1]] = score
+            if small_is_good:
+                if score <= threshold: good_models[name[8:len(name) - 2].split(".")[-1]] = score
+            else:
+                if score >= threshold: good_models[name[8:len(name) - 2].split(".")[-1]] = score
 
-    # read data, build model and do prediction
-    Xtrain, Xtest, ytrain = data_preprocess(train, test)
+        return sorted(good_models.items(), key=operator.itemgetter(1)), all_models
 
-    xgb, param_grid = XGBRegressor(seed=0), \
-                      {
-                          'n_estimators': [300, 600, 900, 1200],
-                           'learning_rate': [ 0.02, 0.04, 0.06, 0.08, 0.1],
-                           'max_depth': [ 7],
-                           'subsample': [ 0.8],
-                           'colsample_bytree': [0.8],
-                      }
+    def print_dict(dd):
+        for k, v in dd.items():
+            print("{} : {}".format(k, v))
 
-    # gbr, param_grid = GradientBoostingRegressor(random_state=0)
+    class ensemble(object):
+        def __init__(self, n_folds, stacker, base_models):
+            self.n_folds = n_folds
+            self.stacker = stacker
+            self.base_models = base_models
+
+        def fit_predict(self, train, test, ytr):
+            X = train.values
+            y = ytr.values
+            T = test.values
+            folds = KFold(n_splits=self.n_folds, shuffle=True, random_state=0)
+            S_train = np.zeros((X.shape[0], len(self.base_models)))
+            S_test = np.zeros((T.shape[0], len(self.base_models)))
+            for i, reg in enumerate(base_models):
+                print("Fitting the base model...")
+                S_test_i = np.zeros((T.shape[0], self.n_folds))
+                for j, (train_idx, test_idx) in enumerate(folds.split(X)):
+                    X_train = X[train_idx]
+                    y_train = y[train_idx]
+                    X_holdout = X[test_idx]
+                    reg.fit(X_train, y_train)
+                    y_pred = reg.predict(X_holdout)[:]
+                    S_train[test_idx, i] = y_pred
+                    S_test_i[:, j] = reg.predict(T)[:]
+                S_test[:, i] = S_test_i.mean(1)
+
+            print("Stacking base models...")
+            # tuning the stacker
+
+            param_grid = {
+                # 'alpha': [i*0.001 for i in range(1, 101, 2)]
+            }
+            grid = GridSearchCV(estimator=self.stacker, param_grid=param_grid, n_jobs=1, cv=5, scoring=RMSE)
+            grid.fit(S_train, y)
+            try:
+                print('Param grid:')
+                print(param_grid)
+                print('Best Params:')
+                print(grid.best_params_)
+                print('Best CV Score:')
+                print(-grid.best_score_)
+                print('Best estimator:')
+                print(grid.best_estimator_)
+            except:
+                pass
+
+            y_pred = grid.predict(S_test)[:]
+            return y_pred, -grid.best_score_
+
+    # build a model library (can be improved)
+    base_models = [
+        RandomForestRegressor(
+            n_jobs=1, random_state=0,
+            n_estimators=500, max_features=14
+        ),
+        RandomForestRegressor(
+            n_jobs=1, random_state=0,
+            n_estimators=500, max_features=20,
+            max_depth=7
+        ),
+        RandomForestRegressor(
+            n_jobs=1, random_state=0,
+            n_estimators=500, max_features='log2'
+        ),
+        RandomForestRegressor(
+            n_jobs=1, random_state=0,
+            n_estimators=500, max_features='sqrt',
+            max_depth=7
+        ),
+        ExtraTreesRegressor(
+            n_jobs=1, random_state=0,
+            n_estimators=500, max_features=15
+        ),
+        ExtraTreesRegressor(
+            n_jobs=1, random_state=0,
+            n_estimators=500, max_features=20
+        ),
+        ExtraTreesRegressor(
+            n_jobs=1, random_state=0,
+            n_estimators=500, max_features='log2'
+        ),
+        ExtraTreesRegressor(
+            n_jobs=1, random_state=0,
+            n_estimators=500, max_features='sqrt'
+        ),
+        GradientBoostingRegressor(
+            random_state=0,
+            n_estimators=500, max_features=10, max_depth=6,
+            learning_rate=0.05, subsample=0.8
+        ),
+        GradientBoostingRegressor(
+            random_state=0,
+            n_estimators=500, max_features=15, max_depth=6,
+            learning_rate=0.05, subsample=0.8
+        ),
+        GradientBoostingRegressor(
+            random_state=0,
+            n_estimators=500, max_features='log2', max_depth=6,
+            learning_rate=0.05, subsample=0.8
+        ),
+        GradientBoostingRegressor(
+            random_state=0,
+            n_estimators=500, max_features='sqrt', max_depth=6,
+            learning_rate=0.05, subsample=0.8
+        ),
+        XGBRegressor(
+            seed=0,
+            n_estimators=500, max_depth=10,
+            learning_rate=0.05, subsample=0.8, colsample_bytree=0.75
+        ),
+
+        XGBRegressor(
+            seed=0,
+            n_estimators=500, max_depth=7,
+            learning_rate=0.05, subsample=0.8, colsample_bytree=0.75
+        ),
+        KNeighborsRegressor(n_neighbors=5),
+        KNeighborsRegressor(n_neighbors=10),
+        KNeighborsRegressor(n_neighbors=15),
+        KNeighborsRegressor(n_neighbors=25),
+        # LassoLarsCV(),
+        ElasticNet(),
+        SVR(),
+        Ridge(),
+        BayesianRidge(),
+        OrthogonalMatchingPursuit(),
+
+        # LinearRegression(),
+        # SVR()
+    ]
+#############################################################################################################
+
+    #
+    # xgb = XGBRegressor(seed=0), \
+    #                   {
+    #                       'n_estimators': range(1000, 2100, 100),
+    #                        'learning_rate': [ 0.02, 0.04, 0.06, 0.08, 0.1, 0.5, 0.8],
+    #                        'max_depth': [ 7],
+    #                        'subsample': [ 0.8],
+    #                        'colsample_bytree': [0.8],
+    #                   }
+    #
+    # gbr = GradientBoostingRegressor(random_state=0), \
+    #                   {
+    #                       'n_estimators': range(1000, 2100, 100),
+    #                       'learning_rate': [0.02, 0.04, 0.06, 0.08, 0.1, 0.5, 0.8],
+    #                       'max_depth': [7],
+    #                       'subsample': [0.8],
+    #                       'colsample_bytree': [0.8],
+    #                   }
+
+    X_train, X_test, y_train = data_preprocess(train, test)
+
+    ensem = ensemble(n_folds=5, stacker=Ridge(), base_models=base_models)
+
+    y_pred, score = ensem.fit_predict(X_train, X_test, y_train)
+
+    # print(-1 * cross_val_score(Ridge(), X_train, y_train,
+    #                          scoring=RMSE, cv=5).mean())
+
+    # res = untuned_model_finder(X_train, y_train, models, RMSE, 5, threshold=0.15)
+    # for elem in res[0]:
+    #     print(elem)
+    # print("========================================================================")
+    # print_dict(res[1])
 
 
-    grid_searcher(Xtrain, Xtest, ytrain, xgb, param_grid)
 
-    # test_predict,score = model_random_forecast(Xtrain,Xtest,ytrain)
-    # test_predict,score = model_xgb_regression(Xtrain,Xtest,ytrain)
+    # grid_searcher(X_train, X_test, y_train, KNeighborsRegressor(), {'n_neighbors': range(1, 101, 1)})
+    # grid_searcher(Xtrain, Xtest, ytrain, gbr[0], gbr[1])
+
+    # test_predict, score = model_random_forecast(Xtrain,Xtest,ytrain)
+    # test_predict, score = model_xgb_regression(Xtrain,Xtest,ytrain)
     # test_predict, score = model_extra_trees_regression(Xtrain, Xtest, ytrain)
-    # test_predict,score = model_gradient_boosting_tree(Xtrain,Xtest,ytrain)
+    # test_predict, score = model_gradient_boosting_tree(Xtrain,Xtest,ytrain)
 
-    # create_submission(np.exp(test_predict), score)
+    create_submission(np.exp(y_pred))
 
 if __name__ == "__main__":
     housing()
