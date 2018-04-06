a89109bafb039303dd1142028520106c88bd3072
dejoyjnr <dejoy@mail.de>
2018-04-06 16:23:19 +0200

    Add files via upload

--- housing.py
+++ housing.py
@@ -4,22 +4,45 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, KFold
 from sklearn.metrics import mean_absolute_error, make_scorer, mean_squared_error
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
-from sklearn.preprocessing import LabelEncoder, Imputer
+from sklearn.preprocessing import LabelEncoder, Imputer, RobustScaler
 from xgboost import XGBRegressor
-from sklearn.base import TransformerMixin
+from sklearn.base import TransformerMixin, BaseEstimator, RegressorMixin, clone
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.ensemble.partial_dependence import partial_dependence, plot_partial_dependence
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import BayesianRidge, LinearRegression, Ridge, Lasso, ElasticNet, Lars, LassoLars, LassoCV, LassoLarsCV
-from sklearn.linear_model import OrthogonalMatchingPursuit, HuberRegressor, RANSACRegressor
+from sklearn.linear_model import OrthogonalMatchingPursuit, HuberRegressor, RANSACRegressor, LassoLarsIC
 from sklearn.preprocessing import PolynomialFeatures
 from scipy.stats import skew
-import operator
+import operator, datetime
 from sklearn.svm import SVR
 from sklearn.neural_network import MLPRegressor
+from sklearn.kernel_ridge import KernelRidge
+import lightgbm as lgb
+from scipy.special import boxcox1p
 
+lasso = make_pipeline(RobustScaler(), Lasso(alpha=0.0005, random_state=1))
+ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10,
+                                   loss='huber', random_state=5)
+model_lgb = lgb.LGBMRegressor(objective='regression', num_leaves=5,
+                              learning_rate=0.05, n_estimators=720,
+                              max_bin=55, bagging_fraction=0.8,
+                              bagging_freq=5, feature_fraction=0.2319,
+                              feature_fraction_seed=9, bagging_seed=9,
+                              min_data_in_leaf=6, min_sum_hessian_in_leaf=11)
+
+model_xgb = XGBRegressor(colsample_bytree=0.4603, gamma=0.0468,
+                             learning_rate=0.05, max_depth=3,
+                             min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571,
+                             subsample=0.5213, silent=1,
+                             random_state =7, nthread = -1)
 
 models = [LinearRegression(),
           Ridge(),
@@ -38,7 +61,8 @@ models = [LinearRegression(),
           GradientBoostingRegressor(),
           XGBRegressor(),
           SVR(),
-          MLPRegressor()]
+          MLPRegressor()
+          ]
 
 def housing():
 
@@ -140,15 +164,18 @@ def housing():
 
     RMSE = make_scorer(mean_squared_error_, greater_is_better=False)
 
-
-    def create_submission(prediction):
-        pd.DataFrame({'Id': test['Id'].values, 'SalePrice': prediction}).to_csv(file_path+'sub.csv', index=False)
+    def create_submission(prediction, score):
+        now = datetime.datetime.now()
+        sub_file = 'submission_' + str(score) + '_' + str(now.strftime("%Y-%m-%d-%H-%M")) + '.csv'
+        # sub_file = 'prediction_training.csv'
+        print('Creating submission: ', sub_file)
+        pd.DataFrame({'Id': test['Id'].values, 'SalePrice': prediction}).to_csv(sub_file, index=False)
 
 
     def data_preprocess(train, test):
-        outlier_idx = [4, 11, 13, 20, 46, 66, 70, 167, 178, 185, 199, 224, 261, 309, 313, 318, 349, 412, 423, 440, 454, 477, 478, 523, 540, 581, 588, 595,
-                       654, 688, 691, 774, 798, 875, 898, 926, 970, 987, 1027, 1109, 1169, 1182, 1239, 1256, 1298, 1324, 1353, 1359, 1405, 1442, 1447]
-        train.drop(train.index[outlier_idx], inplace=True)
+        # outlier_idx = [4, 11, 13, 20, 46, 66, 70, 167, 178, 185, 199, 224, 261, 309, 313, 318, 349, 412, 423, 440, 454, 477, 478, 523, 540, 581, 588, 595,
+        #                654, 688, 691, 774, 798, 875, 898, 926, 970, 987, 1027, 1109, 1169, 1182, 1239, 1256, 1298, 1324, 1353, 1359, 1405, 1442, 1447]
+        # train.drop(train.index[outlier_idx], inplace=True)
         all_data = pd.concat((train.loc[:, 'MSSubClass':'SaleCondition'],
                               test.loc[:, 'MSSubClass':'SaleCondition']))
 
@@ -169,6 +196,71 @@ def housing():
         y_train = train.SalePrice
 
         return X_train, X_test, y_train
+
+    def preprocess_data(train, test):
+        outlier_idx = [4, 11, 13, 20, 46, 66, 70, 167, 178, 185, 199, 224, 261, 309, 313, 318, 349, 412, 423, 440, 454, 477, 478, 523, 540, 581, 588, 595,
+                       654, 688, 691, 774, 798, 875, 898, 926, 970, 987, 1027, 1109, 1169, 1182, 1239, 1256, 1298, 1324, 1353, 1359, 1405, 1442, 1447]
+        train.drop(train.index[outlier_idx], inplace=True)
+        train["SalePrice"] = np.log1p(train["SalePrice"])
+        all_data = pd.concat((train.loc[:, 'MSSubClass':'SaleCondition'],
+                              test.loc[:, 'MSSubClass':'SaleCondition']))
+        all_data["PoolQC"] = all_data["PoolQC"].fillna("None")
+        all_data["MiscFeature"] = all_data["MiscFeature"].fillna("None")
+        all_data["Alley"] = all_data["Alley"].fillna("None")
+        all_data["Fence"] = all_data["Fence"].fillna("None")
+        all_data["FireplaceQu"] = all_data["FireplaceQu"].fillna("None")
+        all_data["LotFrontage"] = all_data.groupby("Neighborhood")["LotFrontage"].transform(
+            lambda x: x.fillna(x.median()))
+        for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
+            all_data[col] = all_data[col].fillna('None')
+        for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
+            all_data[col] = all_data[col].fillna(0)
+        for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
+            all_data[col] = all_data[col].fillna(0)
+        for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+            all_data[col] = all_data[col].fillna('None')
+        all_data["MasVnrType"] = all_data["MasVnrType"].fillna("None")
+        all_data["MasVnrArea"] = all_data["MasVnrArea"].fillna(0)
+        all_data['MSZoning'] = all_data['MSZoning'].fillna(all_data['MSZoning'].mode()[0])
+        all_data = all_data.drop(['Utilities'], axis=1)
+        all_data["Functional"] = all_data["Functional"].fillna("Typ")
+        all_data['Electrical'] = all_data['Electrical'].fillna(all_data['Electrical'].mode()[0])
+        all_data['KitchenQual'] = all_data['KitchenQual'].fillna(all_data['KitchenQual'].mode()[0])
+        all_data['Exterior1st'] = all_data['Exterior1st'].fillna(all_data['Exterior1st'].mode()[0])
+        all_data['Exterior2nd'] = all_data['Exterior2nd'].fillna(all_data['Exterior2nd'].mode()[0])
+        all_data['SaleType'] = all_data['SaleType'].fillna(all_data['SaleType'].mode()[0])
+        all_data['MSSubClass'] = all_data['MSSubClass'].fillna("None")
+        all_data['MSSubClass'] = all_data['MSSubClass'].apply(str)
+        all_data['OverallCond'] = all_data['OverallCond'].astype(str)
+        all_data['YrSold'] = all_data['YrSold'].astype(str)
+        all_data['MoSold'] = all_data['MoSold'].astype(str)
+
+        cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
+                'ExterQual', 'ExterCond', 'HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1',
+                'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+                'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond',
+                'YrSold', 'MoSold')
+        for c in cols:
+            lbl = LabelEncoder()
+            lbl.fit(list(all_data[c].values))
+            all_data[c] = lbl.transform(list(all_data[c].values))
+        all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
+        numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+        skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+        skewness = pd.DataFrame({'Skew': skewed_feats})
+        skewness = skewness[abs(skewness) > 0.75]
+        skewed_features = skewness.index
+        lam = 0.15
+        for feat in skewed_features:
+            # all_data[feat] += 1
+            all_data[feat] = boxcox1p(all_data[feat], lam)
+        all_data = pd.get_dummies(all_data)
+        X_train = all_data[:train.shape[0]]
+        X_test = all_data[train.shape[0]:]
+        y_train = train.SalePrice
+
+        return X_train, X_test, y_train
+
 
 
     def model_random_forecast(Xtrain, Xtest, ytrain):
@@ -400,19 +492,22 @@ def housing():
             n_estimators=500, max_depth=7,
             learning_rate=0.05, subsample=0.8, colsample_bytree=0.75
         ),
-        KNeighborsRegressor(n_neighbors=5),
-        KNeighborsRegressor(n_neighbors=10),
-        KNeighborsRegressor(n_neighbors=15),
-        KNeighborsRegressor(n_neighbors=25),
+        # KNeighborsRegressor(n_neighbors=5),
+        # KNeighborsRegressor(n_neighbors=10),
+        # KNeighborsRegressor(n_neighbors=15),
+        # KNeighborsRegressor(n_neighbors=25),
         # LassoLarsCV(),
         ElasticNet(),
         SVR(),
         Ridge(),
         BayesianRidge(),
         OrthogonalMatchingPursuit(),
-
-        # LinearRegression(),
-        # SVR()
+        # ENet,
+        # GBoost,
+        # KRR,
+        # lasso,
+        # model_lgb,
+        # model_xgb
     ]
 #############################################################################################################
 
@@ -435,13 +530,36 @@ def housing():
     #                       'colsample_bytree': [0.8],
     #                   }
 
-    X_train, X_test, y_train = data_preprocess(train, test)
+    X_train, X_test, y_train = preprocess_data(train, test)
 
     ensem = ensemble(n_folds=5, stacker=Ridge(), base_models=base_models)
 
     y_pred, score = ensem.fit_predict(X_train, X_test, y_train)
 
-    # print(-1 * cross_val_score(Ridge(), X_train, y_train,
+    class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+        def __init__(self, models):
+            self.models = models
+
+        # we define clones of the original models to fit the data in
+        def fit(self, X, y):
+            self.models_ = [clone(x) for x in self.models]
+
+            # Train cloned base models
+            for model in self.models_:
+                model.fit(X, y)
+
+            return self
+
+        # Now we do the predictions for cloned models and average them
+        def predict(self, X):
+            predictions = np.column_stack([
+                model.predict(X) for model in self.models_
+            ])
+            return np.mean(predictions, axis=1)
+
+    averaged_models = AveragingModels(models=(ENet, GBoost, KRR, lasso))
+
+    # print(-1 * cross_val_score(averaged_models, X_train, y_train,
     #                          scoring=RMSE, cv=5).mean())
 
     # res = untuned_model_finder(X_train, y_train, models, RMSE, 5, threshold=0.15)
@@ -460,7 +578,9 @@ def housing():
     # test_predict, score = model_extra_trees_regression(Xtrain, Xtest, ytrain)
     # test_predict, score = model_gradient_boosting_tree(Xtrain,Xtest,ytrain)
 
-    create_submission(np.exp(y_pred))
+    # create_submission(np.exp(y_pred), score)
+
+
 
 if __name__ == "__main__":
     housing()
